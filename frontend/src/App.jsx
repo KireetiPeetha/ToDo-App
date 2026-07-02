7babfcf4899a24ef07aa266347008f8e7e3c0d72
Kireeti Peetha <kireetipeetha02@gmail.com>
2026-07-02 15:20:53 +0530

    Fetch and display todos from backend in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 function App() {
-    return <h1>Hello, ToDo App</h1>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/get_all_todos")
+      .then(response => response.json())
+      .then(data => setTodos(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>ToDo App</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default App;
