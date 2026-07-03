fd0e56bcd9f1dc73c635332c1248941e5df4d5bc
Kireeti Peetha <kireetipeetha02@gmail.com>
2026-07-03 13:42:49 +0530

    Fix create and update todo to save all new fields

--- backend/services.py
+++ backend/services.py
@@ -10,6 +10,13 @@ def create_todo_service(todo_data: ToDoCreate, current_user):
         new_todo = ToDo(
             title=todo_data.title,
             description=todo_data.description,
+            due_date=todo_data.due_date,
+            due_time=todo_data.due_time,
+            priority=todo_data.priority,
+            repeat=todo_data.repeat,
+            reminder=todo_data.reminder,
+            is_important=todo_data.is_important,
+            category_id=todo_data.category_id,
             user_id=current_user.id
         )
         db.add(new_todo)
@@ -49,6 +56,20 @@ def update_todo_service(todo_id: UUID, todo_data: ToDoUpdate, current_user):
                 todo.description = todo_data.description
             if todo_data.completed is not None:
                 todo.completed = todo_data.completed
+            if todo_data.is_important is not None:
+                todo.is_important = todo_data.is_important
+            if todo_data.due_date is not None:
+                todo.due_date = todo_data.due_date
+            if todo_data.due_time is not None:
+                todo.due_time = todo_data.due_time
+            if todo_data.priority is not None:
+                todo.priority = todo_data.priority
+            if todo_data.repeat is not None:
+                todo.repeat = todo_data.repeat
+            if todo_data.reminder is not None:
+                todo.reminder = todo_data.reminder
+            if todo_data.category_id is not None:
+                todo.category_id = todo_data.category_id
             db.commit()
             db.refresh(todo)
             return todo
